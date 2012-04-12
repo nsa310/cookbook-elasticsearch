41eab09f972c1bd99dd918a28a6c179a1afbffcc
Nitish Sharma <nitish@skylin.es>
2012-04-12 16:32:36 +0200

    switching to ES deb for installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,33 +4,6 @@ elasticsearch = "elasticsearch-#{node.elasticsearch[:version]}"
 #
 include_recipe "elasticsearch::curl"
 
-# Create user and group
-#
-group node.elasticsearch[:user] do
-  action :create
-end
-
-user node.elasticsearch[:user] do
-  comment "ElasticSearch User"
-  home    "#{node.elasticsearch[:dir]}/elasticsearch"
-  shell   "/bin/bash"
-  gid     node.elasticsearch[:user]
-  supports :manage_home => false
-  action  :create
-end
-
-# FIX: Work around the fact that Chef creates the directory even for `manage_home: false`
-bash "remove the elasticsearch user home" do
-  user    'root'
-  code    "rm -rf  #{node.elasticsearch[:dir]}/elasticsearch"
-  only_if "test -d #{node.elasticsearch[:dir]}/elasticsearch"
-end
-
-directory "#{node.elasticsearch[:dir]}/elasticsearch" do
-  owner "root"
-  group "root"
-end
-
 # Increase open file limits
 #
 bash "enable user limits" do
@@ -56,91 +29,35 @@ end
 
 # Download ES
 #
-remote_file "/tmp/elasticsearch-#{node.elasticsearch[:version]}.tar.gz" do
-  source "https://github.com/downloads/elasticsearch/elasticsearch/#{elasticsearch}.tar.gz"
+remote_file "/tmp/elasticsearch-#{node.elasticsearch[:version]}.deb" do
+  source "https://github.com/downloads/elasticsearch/elasticsearch/#{elasticsearch}.deb"
   action :create_if_missing
 end
 
-# Move to ES dir
+# Install ES
 #
-bash "Move elasticsearch to #{node.elasticsearch[:dir]}/elasticsearch" do
+bash "Install Elasticsearch" do
   user "root"
   cwd  "/tmp"
 
   code <<-EOS
-    tar xfz /tmp/#{elasticsearch}.tar.gz
-    mv --force /tmp/#{elasticsearch}/* #{node.elasticsearch[:dir]}/elasticsearch/
-  EOS
-
-  creates "#{node.elasticsearch[:dir]}/elasticsearch/lib/#{elasticsearch}.jar"
-  creates "#{node.elasticsearch[:dir]}/elasticsearch/bin/elasticsearch"
-end
-
-#Download ES service wrapper
-#
-remote_file "/tmp/service.tar.gz" do
-  source "http://github.com/elasticsearch/elasticsearch-servicewrapper/tarball/master"
-  action :create_if_missing
-end
-
-#Move service wrapper
-#
-bash "Move ES service wrapper to #{node.elasticsearch[:dir]}/elasticsearch/bin" do
-  user "root"
-
-  code <<-EOS
-    tar xzf /tmp/service.tar.gz
-    mv --force *servicewrapper*/service #{node.elasticsearch[:dir]}/elasticsearch/bin/
-  EOS
-end
-
-#Modify ES init script in service wrapper
-#
-template "#{node.elasticsearch[:dir]}/elasticsearch/bin/service/elasticsearch" do
-  source "elasticsearch.init.erb"
-  owner 'root' and mode 0755
-end
-
-#Modify ES configuration of service wrapper
-#
-template "#{node.elasticsearch[:dir]}/elasticsearch/bin/service/elasticsearch.conf" do
-  source "elasticsearch-service.conf.erb"
-  owner 'root' and mode 0755
-end
-
-# Create service
-#
-bash "Install ES service wrapper from #{node.elasticsearch[:dir]}/elasticsearch/bin/service/elasticsearch" do
-  user "root"
-  code <<-EOS
-    #{node.elasticsearch[:dir]}/elasticsearch/bin/service/elasticsearch install
-  EOS
-end
-
-#Create lock dir
-#
-bash "create ES lock dir" do
-  user "root"
-  code <<-EOS
-     mkdir #{node.elasticsearch[:dir]}/elasticsearch/lock
-  EOS
-end
-
-# Ensure proper permissions
-#
-bash "Ensure proper permissions for #{node.elasticsearch[:dir]}/elasticsearch" do
-  user    "root"
-  code    <<-EOS
-    chown -R #{node.elasticsearch[:user]}:#{node.elasticsearch[:user]} #{node.elasticsearch[:dir]}/elasticsearch
-    chmod -R 775 #{node.elasticsearch[:dir]}/elasticsearch
+    sudo dpkg -i /tmp/#{elasticsearch}.tar.gz
   EOS
 end
 
 # Create ES config file
 #
 template "elasticsearch.yml" do
-  path   "#{node.elasticsearch[:dir]}/elasticsearch/config/elasticsearch.yml"
+  path   "/etc/elasticsearch/elasticsearch.yml"
   source "elasticsearch.yml.erb"
+  owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
+end
+
+# Create ES default file
+#
+template "elasticsearch" do
+  path "/etc/default/elasticsearch"
+  source "elasticsearch.default.erb"
   owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
 end
 
